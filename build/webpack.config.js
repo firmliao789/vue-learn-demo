e1ec98c28ef9aad8fadd760b7e851249c6cad9c8
廖毅 <firmliao789@126.com>
2019-01-14 13:22:39 +0800

    css完成配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const glob = require('glob');
+const PurifyCSSPlugin = require("purifycss-webpack");
+
 const resolve = (file) => {
     return path.join(__dirname, '..', file);
 }
@@ -20,9 +24,69 @@ module.exports = {
             {
                 test: /\.js/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {presets: ['@babel/preset-env']}
+                    loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.(png|jpg)/,
+                use: [{
+                    'loader': 'url-loader',
+                    options: {
+                        limit: 100,
+                        name: 'img/[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.css/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {importLoaders: 1}
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: './build/postcss.config.js'
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {importLoaders: 1}
+                    },
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: './build/postcss.config.js'
+                            }
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -31,7 +95,17 @@ module.exports = {
             template: './index.html'
         }),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "css/[name].[contenthash].css",
+            chunkFilename: "[id].css"
+        }),
+        new PurifyCSSPlugin({
+            //这里配置了一个paths，主要是需找html模板，purifycss根据这个配置会遍历你的文件，查找哪些css被使用了。
+            paths: glob.sync(path.join(__dirname, '..','index.html')),
+        })
     ],
     resolve: {
         extensions: ['.js'],
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import myVue from './js/myVue'
+import myVue from './js/myVue';
+import './css/style.css'
+import './css/style.scss'
 
 window.myVue = myVue;
+
+const img = require('./assets/img201709231029240_info300X300.jpg')
+
+console.log(img);
